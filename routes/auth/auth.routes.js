d0db9be67d00df2350b19e99f9b8d99ffa8ca909
Inês Almeida <ines_almeida_203@hotmail.com>
2023-06-24 18:53:34 +0100

    signup page login button and error message

--- routes/auth/auth.routes.js
+++ routes/auth/auth.routes.js
@@ -29,7 +29,7 @@ router.post("/signup", async (req, res, next) => {
 });
 
 
-router.get('/login', isLoggedIn, (req, res) =>{
+router.get('/login', isLoggedOut, (req, res) =>{
   
       res.render('auth/login');
   
