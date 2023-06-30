e24d78e92164b6f54286460a3c216241fd8ef65e
Rinan <jandrewschnell@gmail.com>
2023-06-30 13:03:14 +0200

    edit comment

--- routes/post/post.routes.js
+++ routes/post/post.routes.js
@@ -37,47 +37,24 @@ router.get("/post/:id", async (req, res, next) => {
     const postId = req.params.id;
     const post = await Post.findById(postId).populate('author');
     const loadComments = await Comment.find({Post: postId}).populate('author');
-    for(i=0; i < loadComments.length ;i++){
-      if(loadComments[i].author.username === req.session.currentUser.username){loadComments[i].owner = true;}
-    }
-    const confirmUser = await Post.findById(req.params.id).populate('author')
-    if (confirmUser.author.id === req.session.currentUser._id)
-    {
-      res.render('post/postPage', { post: post, comment: loadComments, isOwner: true, currentUser: req.session.currentUser });
-    }
-    else if (req.session.currentUser)
-    {
-      res.render('post/postPage', { post: post, comment: loadComments, currentUser: req.session.currentUser });
-    } else {
+    if(!req.session.currentUser){
       res.render('post/postPage', { post: post, comment: loadComments });
+    }
+    else if (req.session.currentUser){
+      for(i=0; i < loadComments.length ;i++){
+        if(loadComments[i].author.username === req.session.currentUser.username){loadComments[i].owner = true;}
+      }
+      if(req.session.currentUser.username === post.author.username){
+        res.render('post/postPage', { post: post, comment: loadComments, isOwner: true, currentUser: req.session.currentUser });  
+      }
+      else{res.render('post/postPage', { post: post, comment: loadComments, currentUser: req.session.currentUser });}
     }
   } catch (error) {
     next(error);
   }
 });
 
-router.post("/post/:id/comment", async (req, res, next) => {
-  try {
-    const userinQuestion = await User.findOne({username: req.session.currentUser.username});
-    if(req.session.currentUser){
-      await Comment.create({
-        author: userinQuestion._id,
-        Post: req.params.id,
-        content: req.body.content,
-      })
-      const currentPost = await Post.findById(req.params.id);
-      // console.log(currentPost)
-      const commentIncrease = currentPost.commentcount + 1;
-      // console.log(commentIncrease)
-      await Post.findByIdAndUpdate(req.params.id, {commentcount: commentIncrease});
-      res.redirect(`/post/${req.params.id}`)
-    }
-    else {res.redirect("/");}
-  }
-  catch (error) {
-    next(error);
-  }
-});
+
 
 router.get("/post/:id/edit", async (req, res, next) => {
   try {
@@ -110,6 +87,46 @@ router.post("/post/:id/edit", async (req, res, next) => {
       res.redirect("/");
     }
   } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/post/:id/comment", async (req, res, next) => {
+  try {
+    const userinQuestion = await User.findOne({username: req.session.currentUser.username});
+    if(req.session.currentUser){
+      await Comment.create({
+        author: userinQuestion._id,
+        Post: req.params.id,
+        content: req.body.content,
+      })
+      const post = await Post.findById(req.params.id);
+      // console.log(post)
+      const commentIncrease = post.commentcount + 1;
+      // console.log(commentIncrease)
+      await Post.findByIdAndUpdate(req.params.id, {commentcount: commentIncrease});
+      res.redirect(`/post/${req.params.id}`)
+    }
+    else {res.redirect("/");}
+  }
+  catch (error) {
+    next(error);
+  }
+});
+
+router.post("/comment/:id", async (req, res, next) => {
+  try {
+    const commentFind = await Comment.findById(req.params.id).populate('author').populate('Post');
+    if(req.session.currentUser){
+      if(req.session.currentUser.username === commentFind.author.username){
+        await Comment.findByIdAndUpdate(req.params.id, req.body)
+        res.redirect(`/post/${commentFind.Post.id}`)
+      }
+      res.redirect("/");
+    }
+    else {res.redirect("/");}
+  }
+  catch (error) {
     next(error);
   }
 });
