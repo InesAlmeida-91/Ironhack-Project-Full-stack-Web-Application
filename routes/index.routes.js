c8fd70d73f953c5d7b4ecadc07bd3cfdffa1302e
Rinan <jandrewschnell@gmail.com>
2023-07-01 13:36:26 +0200

    changed api into random useless facts

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -7,17 +7,18 @@ const axios = require('axios');
 router.get("/", async (req, res, next) => {
   try{
 
-    // const response = await axios.get(`https://api.thenewsapi.com/v1/news/top?api_token=pFyFZryDpCem6wnekpOx2nrMsfpAlLQAxbeG0SyK&locale=us&limit=3`);
-    // const news = response.data.data;
+    const todaysFacts = await axios.get(`https://uselessfacts.jsph.pl/api/v2/facts/today`);
+    const randomFacts = await axios.get(`https://uselessfacts.jsph.pl/api/v2/facts/random`);
+    const randomFactsde = await axios.get(`https://uselessfacts.jsph.pl/api/v2/facts/random?language=de`);
     const postArray = await Post.find().populate('author')
     const latestFirst = []; 
     postArray.forEach(element => {
       latestFirst.unshift(element)
     });
     if(req.session.currentUser){
-      res.render('index', {postArray: latestFirst, currentUser: req.session.currentUser});
+      res.render('index', {postArray: latestFirst, currentUser: req.session.currentUser, fact1: todaysFacts.data, fact2: randomFacts.data, fact3: randomFactsde.data});
     }
-    else {res.render("index", {postArray: latestFirst});}
+    else {res.render("index", {postArray: latestFirst, fact1: todaysFacts.data, fact2: randomFacts.data, fact3: randomFactsde.data});}
   }
   catch(error){
     res.render('auth/signup', {errorMessage: 'invalid username or email'})
